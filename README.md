659afe14460312c78d83d4b9df4c2e25888fda61
gergo <kiss.gergely@gmail.com>
2025-04-28 00:14:59 +0200

    handling Aquara T1 temp sensors

--- README.md
+++ README.md
@@ -5,6 +5,23 @@
 * https://smlight.tech/product/slzb-07p7
 * Chipset: CC2652P7
 * Baud rate of the factory FW is 115200
+* Firmware upgrade: https://smlight.tech/flasher/#SLZB-07
+
+**IMPORTANT**: UPGRADE THE FIRMWARE. I struggled a lot with zigpy before I figured out my FW was older than what zigpy expected (facepalm).
+
+## Sensors
+
+### Aqara Temperature and humidity sensor T1
+
+* Device name `lumi.sensor_ht.agl02`
+* [Link](https://zigbee.blakadder.com/Aqara_WSDCGQ12LM.html)
+
+## Dev setup
+
+```
+python3 -m venv .venv
+source .venv/bin/activate
+```
 
 ## ZigPy
 
@@ -20,13 +37,6 @@ For some reason, SLZB-07p7 works with ezsp (bellows), but not with znp, even tho
 
 So the library we need is [zigpy/bellows](https://github.com/zigpy/bellows) specifically, but it turns out we need `zigpy-cli` to invoke eg. `energy-scan`.
 
-## Setup
-
-```
-python3 -m venv .venv
-source .venv/bin/activate
-```
-
 ### Forming a network
 
 * Set the device: `export EZSP_DEVICE=/dev/ttyUSB0`
@@ -35,16 +45,18 @@ source .venv/bin/activate
 * (Pick the most quiet channel)
 * Change the channel: `zigpy radio ezsp $EZSP_DEVICE change-channel -c <channel>`
 * Form a network: `zigpy radio ezsp $EZSP_DEVICE form`
-* Allow pairing: `zigpy -v radio ezsp $EZSP_DEVICE permit -t 60`
+* Allow pairing: `zigpy -v radio --database zigbee.db ezsp $EZSP_DEVICE permit -t 60`
 * (Reset/pair your end devices in this timeframe)
-* Verify the paired devices: `zigpy radio --database srvu.db ezsp $EZSP_DEVICE backup` (look for the `devices` section at the end)
+* Verify the paired devices: `zigpy radio ezsp $EZSP_DEVICE backup` (look for the `devices` section at the end)
 
-Note: the available options make you think you need a `--database`, but you actually don't. This particular coordinator remembers paired devices even when restarted.
+Note: the coordinator remembers joined devices, so I'm not 100% sure we actually need the `--database` option.
+
+**IMPORTANT**: you need to re-form the network after firmware upgrades!
 
 ## Links
 
 * https://github.com/zigpy
 * https://smlight.tech/manual-slzb-07/
 * https://github.com/zigpy/zigpy-cli?tab=readme-ov-file#forming-a-network
-* https://github.com/zigpy/zigpy/issues/452
-
+* https://github.com/zigpy/zigpy/issues/1087
+* https://github.com/zigpy/zigpy/blob/dev/CONTRIBUTING.md#the-zigpy-api
--- zigbee-listener.py
+++ zigbee-listener.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
-import asyncio
 from bellows.zigbee.application import ControllerApplication
+from zigpy.zcl.clusters.measurement import PressureMeasurement, RelativeHumidity, TemperatureMeasurement
+import asyncio
+import logging
+import time
+import zigpy
 
 
 class MainListener:
@@ -9,13 +13,47 @@ class MainListener:
     Contains callbacks that zigpy will call whenever something happens.
     Look for `listener_event` in the Zigpy source or just look at the logged warnings.
     '''
-    def device_joined(self, device):
-        print(f'device_joined: {device}')
 
-    def device_initialized(self, device):
-        print(f'device_initialized {device}')
+    # Application
+
+    def raw_device_initialized(self, dev):
+        logging.debug(f'raw_device_initialized {dev}')
+
+    def device_initialized(self, dev):
+        logging.info(f'device_initialized name={dev.name}, ieee={dev.ieee}, model={dev.model}, endpoints={dev.endpoints}')
+
+    def device_removed(self, dev):
+        logging.info(f'device_removed: {dev.name}')
+
+    def device_joined(self, dev: zigpy.device.Device):
+        logging.info(f'device_joined: name={dev.name}, ieee={dev.ieee}, model={dev.model}')
+    
+    def handle_message(self,
+                       dev: zigpy.device.Device,
+                       profile_id: int,
+                       cluster_id: int,
+                       src_ep: int,
+                       dst_ep: int,
+                       data: bytes):
+        logging.debug(f'handle_message: device={dev}, src_ep={src_ep}, dst_ep={dst_ep}, data={data}')
+
+        for ep in dev.endpoints.values():
+            if type(ep) != zigpy.endpoint.Endpoint: continue
+            for clus in ep.in_clusters.values():
+                match clus:
+                    case PressureMeasurement(): pressure_kPa = clus.get('measured_value') / 1000
+                    case RelativeHumidity(): humidity_pcnt = clus.get('measured_value') / 100
+                    case TemperatureMeasurement(): temp_c = clus.get('measured_value') / 100
+                    case _: logging.debug(f'Unknown: {clus}')
+        logging.info(f'pressure={pressure_kPa}kPa, humidity={humidity_pcnt}%, temp={temp_c}C')
 
 async def main():
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%dT%H:%M:%SZ')
+    logging.Formatter.converter = time.gmtime
+
     app = await ControllerApplication.new(config={
         'database_path': 'zigbee.db',
         'device': {
@@ -26,8 +64,12 @@ async def main():
     listener = MainListener()
     app.add_listener(listener)
 
-    print('Listening...')
-    await asyncio.get_running_loop().create_future()
+    logging.info('Listening...')
+    try:
+        await asyncio.get_running_loop().create_future()
+    except asyncio.exceptions.CancelledError:
+        logging.info('Shutting down...')
+        await app.shutdown()
 
 if __name__ == '__main__':
     asyncio.run(main())
